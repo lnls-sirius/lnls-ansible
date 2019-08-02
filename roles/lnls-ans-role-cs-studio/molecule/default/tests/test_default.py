c8286739deef4b78c4d3ecac63a28fc475bda6ee
Lucas Russo <lerwys@gmail.com>
2019-08-02 12:36:33 -0300

    lnls-ans-role-cs-studio: remove unused test for conda

--- roles/lnls-ans-role-cs-studio/molecule/default/tests/test_default.py
+++ roles/lnls-ans-role-cs-studio/molecule/default/tests/test_default.py
@@ -7,6 +7,5 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('default_group')
 
 
-def test_import_connect2j(host):
-    cmd = host.run('/opt/conda/envs/csstudio/bin/python -c "import connect2j"')
-    assert cmd.rc == 0
+def test_placeholder(host):
+    assert True
