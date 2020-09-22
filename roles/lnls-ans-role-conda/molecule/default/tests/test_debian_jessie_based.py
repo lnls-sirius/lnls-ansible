d1566d97313808433fd1b62eb7ece51693fa267a
carneirofc <claudiofcarneiro@hotmail.com>
2020-09-22 08:48:57 -0300

    role: lnls-ans-role-conda, initial commit.

---
+++ roles/lnls-ans-role-conda/molecule/default/tests/test_debian_jessie_based.py
@@ -0,0 +1,16 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('debian_jessie_based')
+
+
+@pytest.mark.parametrize('repo', [
+    'https://repo.anaconda.com/pkgs/misc/debrepo/conda stable/main',
+])
+def test_repos(host, repo):
+    cmd = host.run('apt-cache policy | grep http | awk \'{print $2,$3}\' | sort -u')
+
+    assert repo in cmd.stdout
