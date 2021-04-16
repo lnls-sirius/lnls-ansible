3f2513c8dadfff3244635c93c75da6cf2706f761
carneirofc <claudiofcarneiro@hotmail.com>
2021-04-16 13:02:55 -0300

    Fix makefile playbook name

--- Makefile_control_room.mk
+++ Makefile_control_room.mk
@@ -1,7 +1,7 @@
-control-room-setup-glusterfs: playbook-control-glusterfs.yml
-	                        ansible-playbook \
-								--user server\
-								--inventory hosts\
-								--ask-pass\
-								--ask-become-pass\
-								playbook-control-glusterfs.yml
+control-room-setup-glusterfs: playbook-control-room-glusterfs.yml
+	ansible-playbook \
+		--user server\
+		--inventory hosts\
+		--ask-pass\
+		--ask-become-pass\
+		playbook-control-room-glusterfs.yml
