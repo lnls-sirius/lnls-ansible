df7f33db28ff60a6a1107ca3a424260cf9c6a854
carneirofc <claudiofcarneiro@hotmail.com>
2021-04-20 11:56:27 -0300

    make/targets/ fix deprecated '-i hosts'

--- make/targets/control_room.mk
+++ make/targets/control_room.mk
@@ -1,7 +1,7 @@
 control-room-setup-glusterfs: playbooks/playbook-control-room-glusterfs.yml
 	ansible-playbook \
 		--user server\
-		--inventory hosts\
+		--inventory $(SIRIUS_INVENTORY)\
 		--ask-pass\
 		--ask-become-pass\
 		playbooks/playbook-control-room-glusterfs.yml
--- make/targets/server.mk
+++ make/targets/server.mk
@@ -1,8 +1,14 @@
+SERVER_OPTS =\
+	--user server\
+	--inventory $(SIRIUS_INVENTORY)\
+	--ask-pass\
+	--ask-become-pass
+
 server-commons-update: playbooks/servers/commons.yml
-	        ansible-playbook --user server --inventory hosts --ask-pass --ask-become-pass playbooks/servers/commons.yml
+	        ansible-playbook $(SERVER_OPTS) playbooks/servers/commons.yml
 
 server-gluster-update: playbooks/servers/gluster.yml
-	        ansible-playbook --user server --inventory hosts --ask-pass --ask-become-pass playbooks/servers/gluster.yml
+	        ansible-playbook $(SERVER_OPTS) playbooks/servers/gluster.yml
 
 server-docker-update: playbooks/servers/docker.yml
-	        ansible-playbook --user server --inventory hosts --ask-pass --ask-become-pass playbooks/servers/docker.yml
+	        ansible-playbook $(SERVER_OPTS) playbooks/servers/docker.yml
