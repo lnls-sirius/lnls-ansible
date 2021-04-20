4e29050df2a27c4b42fc7e8e5f2f04e7f3989826
carneirofc <claudiofcarneiro@hotmail.com>
2021-04-19 22:15:57 -0300

    target/server/ fix typo

--- make/targets/server.mk
+++ make/targets/server.mk
@@ -1,8 +1,8 @@
 server-commons-update: playbooks/servers/commons.yml
-	        ansible-playbook --user server --inventory hosts --ask-pass --ask-become-pass playbooks/servers/-commons.yml
+	        ansible-playbook --user server --inventory hosts --ask-pass --ask-become-pass playbooks/servers/commons.yml
 
 server-gluster-update: playbooks/servers/gluster.yml
-	        ansible-playbook --user server --inventory hosts --ask-pass --ask-become-pass playbooks/servers/-gluster.yml
+	        ansible-playbook --user server --inventory hosts --ask-pass --ask-become-pass playbooks/servers/gluster.yml
 
 server-docker-update: playbooks/servers/docker.yml
-	        ansible-playbook --user server --inventory hosts --ask-pass --ask-become-pass playbooks/servers/-docker.yml
+	        ansible-playbook --user server --inventory hosts --ask-pass --ask-become-pass playbooks/servers/docker.yml
