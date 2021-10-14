a40056402b215143ed4cdc31b0ba837a3bf76454
ximenes <xresende@gmail.com>
2021-10-14 19:18:42 -0300

    Fix inventory paths

--- make/targets/generic.mk
+++ make/targets/generic.mk
@@ -1,16 +1,12 @@
 playbook-beaglebones-repos-checkout.yml: $(PLAYBOOKS_DIR)/beaglebones/repos-checkout.yml
-	ansible-playbook $(EXTRA_OPTS)
+	ansible-playbook $(EXTRA_OPTS) --extra-vars "inventory=$(BEAGLEBONE_INVENTORY)"
 
 playbook-generic-setup-ssh-key.yml: $(PLAYBOOKS_DIR)/generic/setup-ssh-key.yml
 	ansible-playbook $(EXTRA_OPTS)
-playbook-playbook-ctrl-service.yml: $(PLAYBOOKS_DIR)/playbook-ctrl-service.yml
-	ansible-playbook $(EXTRA_OPTS)
-playbook-playbook-desktops.yml: $(PLAYBOOKS_DIR)/playbook-desktops.yml
-	ansible-playbook $(EXTRA_OPTS)
 
 playbook-servers-ioc.yml: $(PLAYBOOKS_DIR)/servers/ioc.yml
-	ansible-playbook $(EXTRA_OPTS)
+	ansible-playbook $(EXTRA_OPTS) --extra-vars "inventory=$(SIRIUS_INVENTORY)"
 playbook-servers-nfs.yml: $(PLAYBOOKS_DIR)/servers/nfs.yml
-	ansible-playbook $(EXTRA_OPTS)
+	ansible-playbook $(EXTRA_OPTS) --extra-vars "inventory=$(SIRIUS_INVENTORY)"
 playbook-servers-web.yml: $(PLAYBOOKS_DIR)/servers/web.yml
-	ansible-playbook $(EXTRA_OPTS)
+	ansible-playbook $(EXTRA_OPTS) --extra-vars "inventory=$(SIRIUS_INVENTORY)"
